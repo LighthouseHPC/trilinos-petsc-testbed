5f685edc6d6665d783c67cdd415bee6ec10fb86f
Pate Motter <patemotter@gmail.com>
2016-01-29 13:40:50 -0700

    Committed wrong setup.py file

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ import errno
 
 download_progress = 0
 percentage = 0
+LIBDIR = ''
 
 
 def dl_progress(block_no, block_size, file_size):
@@ -27,14 +28,14 @@ def download_gcc():
     print('Downloaded gcc 5.3.0')
 
 
-def download_mvapich2():
+def download_openmpi():
     global percentage
     percentage = 0
-    print('Downloading mvapich2 2.2b')
+    print('Downloading openmpi 1.10.2')
     urllib.urlretrieve(
-        'http://mvapich.cse.ohio-state.edu/download/mvapich/mv2/mvapich2-2.2b.tar.gz',
-        'mvapich2-2.2b.tar.gz', reporthook=dl_progress)
-    print('Downloaded mvapich2 2.2b')
+        'http://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-1.10.2.tar.gz',
+        'openmpi-1.10.2.tar.gz', reporthook=dl_progress)
+    print('Downloaded openmpi 1.10.2') 
 
 
 def download_lapack():
@@ -65,6 +66,14 @@ def download_trilinos():
         'trilinos-12.4.2.tar.bz2', reporthook=dl_progress)
     print('Downloaded Trilinos 12.4.2')
 
+def download_petsc():
+    global percentage
+    percentage = 0
+    print('Downloading PETSc 3.6.3')
+    urllib.urlretrieve(
+        'http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.6.3.tar.gz',
+        'petsc-3.6.3.tar.gz', reporthook=dl_progress)
+    print('Downloaded PETSc 3.6.3')
 
 def extract_gcc():
     print('Extracting gcc 5.3.0')
@@ -78,16 +87,16 @@ def extract_gcc():
     print('Extracted gcc 5.3.0')
 
 
-def extract_mvapich2():
-    print('Extracting mvapich2 2.2b')
+def extract_openmpi():
+    print('Extracting openmpi 1.10.2')
     try:
-        mvapich2_tar = tarfile.open('mvapich2-2.2b.tar.gz')
+        openmpi_tar = tarfile.open('openmpi-1.10.2.tar.gz')
     except:
-        print('Mvapich2 tar file does not exist')
+        print('openmpi tar file does not exist')
         exit()
-    mvapich2_tar.extractall()
-    mvapich2_tar.close()
-    print('Extracted mvapich2 2.2b')
+    openmpi_tar.extractall()
+    openmpi_tar.close()
+    print('Extracted openmpi 1.10.2')
 
 
 def extract_trilinos():
@@ -124,6 +133,18 @@ def extract_lapack():
     trilinos_tar.extractall()
     trilinos_tar.close()
     print('Extracted LAPACK 3.6.0')
+
+
+def extract_petsc():
+    print('Extracting PETSc 3.6.3')
+    try:
+        trilinos_tar = tarfile.open('petsc-3.6.3.tar.gz')
+    except:
+        print('PETSc tar file does not exist')
+        exit()
+    trilinos_tar.extractall()
+    trilinos_tar.close()
+    print('Extracted PETSc 3.6.3')
 
 
 # Install functions
@@ -164,16 +185,16 @@ def install_gcc():
     os.chdir('..')
 
 
-def install_mvapich2():
+def install_openmpi():
     try:
-        os.mkdir('mvapich2-install')
+        os.mkdir('openmpi-install')
     except:
         pass
     try:
-        os.chdir('mvapich2-2.2b')
+        os.chdir('openmpi-1.10.2')
     except:
-        print('Mvapich2 has either not been extracted or the source files' +
-              ' are not located in mvapich2-2.2b')
+        print('openmpi has either not been extracted or the source files' +
+              ' are not located in openmpi-1.10.2')
         exit()
     gcc_path = os.path.abspath('../gcc-install')
     install_cmd = ('./configure' +
@@ -181,13 +202,13 @@ def install_mvapich2():
                    ' CXX=' + gcc_path + '/bin/g++' +
                    ' FC=' + gcc_path + '/bin/gfortran' +
                    ' LDFLAGS=-Wl,-rpath,'+ gcc_path + '/lib64' +
-                   ' --prefix=' + os.path.abspath('../mvapich2-install'))
+                   ' --prefix=' + os.path.abspath('../openmpi-install'))
     subprocess.call(install_cmd, shell=True)
-    print('mvapich2 configured')
+    print('openmpi configured')
     subprocess.call('make -j12', shell=True)
-    print('mvapich2 made')
+    print('openmpi made')
     subprocess.call('make install', shell=True)
-    print('mvapich2 installed')
+    print('openmpi installed')
     os.chdir('..')
 
 
@@ -247,7 +268,7 @@ def install_boost():
 
 def install_trilinos():
     gcc_dir = os.path.abspath('./gcc-install')
-    mvapich2_dir = os.path.abspath('./mvapich2-install')
+    openmpi_dir = os.path.abspath('./openmpi-install')
     lapack_dir = os.path.abspath('./lapack-3.6.0')
     boost_dir = os.path.abspath('./boost-install')
     try:
@@ -267,7 +288,7 @@ def install_trilinos():
         f.seek(0)
         f.write(first_line + '\n')
         f.write('GCC=' + gcc_dir + '\n')
-        f.write('MVAPICH=' + mvapich2_dir + '\n')
+        f.write('OPENMPI=' + openmpi_dir + '\n')
         f.write('LAPACK=' + lapack_dir + '\n')
         f.write('BOOST=' + boost_dir + '\n')
         f.writelines(lines)
@@ -276,69 +297,97 @@ def install_trilinos():
     subprocess.call('make install', shell=True)
 
 
+def install_petsc():
+    gcc_dir = os.path.abspath('./gcc-install')        
+    openmpi_dir = os.path.abspath('./openmpi-install')
+    lapack_dir = os.path.abspath('./lapack-3.6.0')    
+    boost_dir = os.path.abspath('./boost-install')    
+    try:
+        os.mkdir('petsc-install')
+    except:
+        pass
+    try:
+        os.chdir('petsc-3.6.3')
+    except:
+        print('petsc-3.6.3 does not exist')
+        exit()
+    install_cmd = ('./configure' +
+                   ' --with-mpi-dir=' + openmpi_dir +
+                   ' --with-lapack-lib=' + lapack_dir + '/liblapack.a' +
+                   ' --with-blas-lib=' + lapack_dir + '/librefblas.a' +
+                   ' --prefix=' + os.path.abspath('../petsc-install') +
+                   ' --with-shared-libraries=0'
+                  )
+    subprocess.call(install_cmd, shell=True)
+
+
 if __name__ == '__main__':
-    download_choices = ['n'] * 6
-    extract_choices = ['n'] * 6
-    install_choices = ['n'] * 6
+    download_choices = []
+    extract_choices = []
+    install_choices = []
+    LIBDIR = os.path.abspath('./gcc-install/lib64')
+    GCC_PATH = os.path.abspath('./gcc-install/bin')
+    MPI_PATH = os.path.abspath('./openmpi-install/bin')
+    os.environ['LD_LIBRARY_PATH'] = os.environ['LD_LIBRARY_PATH'] + ':' + LIBDIR
+    os.environ['PATH'] = GCC_PATH + ':' + MPI_PATH + ':' + os.environ['PATH'] 
+
+    options = ['gcc', 'openmpi', 'LAPACK', 'Boost', 'Trilinos', 'PETSc']
     #  Downloading
-    download_choices[0] = raw_input('Want to download things?: ')
-    if download_choices[0] == 'y':
-        download_choices[1] = raw_input('Want to download gcc?: ')
-        download_choices[2] = raw_input('Want to download mvapich2?: ')
-        download_choices[3] = raw_input('Want to download LAPACK?: ')
-        download_choices[4] = raw_input('Want to download Boost?: ')
-        download_choices[5] = raw_input('Want to download Trilinos?: ')
+    download = raw_input('Want to download things?: ')
+    if download == 'y':
+        for i in range(len(options)):
+            download_choices.append(raw_input('Want to download ' + options[i]  + '?: '))
 
     # Extraction
-    extract_choices[0] = raw_input('Want to extract things?: ')
-    if extract_choices[0] == 'y':
-        extract_choices[1] = raw_input('Want to extract gcc?: ')
-        extract_choices[2] = raw_input('Want to extract mvapich2?: ')
-        extract_choices[3] = raw_input('Want to extract LAPACK?: ')
-        extract_choices[4] = raw_input('Want to extract Boost?: ')
-        extract_choices[5] = raw_input('Want to extract Trilinos?: ')
+    extract = raw_input('Want to extract things?: ')
+    if extract == 'y':
+        for i in range(len(options)):
+            extract_choices.append(raw_input('Want to extract ' + options[i]  + '?: '))
 
     # Installation
-    install_choices[0] = raw_input('Want to install things?: ')
-    if install_choices[0] == 'y':
-        install_choices[1] = raw_input('Want to install gcc?: ')
-        install_choices[2] = raw_input('Want to install mvapich2?: ')
-        install_choices[3] = raw_input('Want to install LAPACK?: ')
-        install_choices[4] = raw_input('Want to install Boost?: ')
-        install_choices[5] = raw_input('Want to install Trilinos?: ')
+    install = raw_input('Want to install things?: ')
+    if install == 'y':
+        for i in range(len(options)):
+            install_choices.append(raw_input('Want to install ' + options[i]  + '?: '))
 
-    if download_choices[0] == 'y':
-        if download_choices[1] == 'y':
+    if download == 'y':
+        if download_choices[0] == 'y':
             download_gcc()
+        if download_choices[1] == 'y':
+            download_openmpi()
         if download_choices[2] == 'y':
-            download_mvapich2()
-        if download_choices[3] == 'y':
             download_lapack()
-        if download_choices[4] == 'y':
+        if download_choices[3] == 'y':
             download_boost()
-        if download_choices[5] == 'y':
+        if download_choices[4] == 'y':
             download_trilinos()
+        if download_choices[5] == 'y':
+            download_petsc()
 
-    if extract_choices[0] == 'y':
-        if extract_choices[1] == 'y':
+    if extract == 'y':
+        if extract_choices[0] == 'y':
             extract_gcc()
+        if extract_choices[1] == 'y':
+            extract_openmpi()
         if extract_choices[2] == 'y':
-            extract_mvapich2()
-        if extract_choices[3] == 'y':
             extract_lapack()
-        if extract_choices[4] == 'y':
+        if extract_choices[3] == 'y':
             extract_boost()
-        if extract_choices[5] == 'y':
+        if extract_choices[4] == 'y':
             extract_trilinos()
+        if extract_choices[5] == 'y':
+            extract_petsc()
 
-    if install_choices[0] == 'y':
-        if install_choices[1] == 'y':
+    if install == 'y':
+        if install_choices[0] == 'y':
             install_gcc()
+        if install_choices[1] == 'y':
+            install_openmpi()
         if install_choices[2] == 'y':
-            install_mvapich2()
-        if install_choices[3] == 'y':
             install_lapack()
-        if install_choices[4] == 'y':
+        if install_choices[3] == 'y':
             install_boost()
-        if install_choices[5] == 'y':
+        if install_choices[4] == 'y':
             install_trilinos()
+        if install_choices[5] == 'y':
+            install_petsc()
